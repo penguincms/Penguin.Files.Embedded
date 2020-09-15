ca51250c1d9fec398e0d02d6444fa7d4b47e13aa
anloffredo <anloffredo@massageenvy.com>
2020-09-15 08:45:52 -0700

    Auto Commit

---
+++ EmbeddedFileService.cs
@@ -0,0 +1,89 @@
+﻿using Penguin.Files.Embedded.Extensions;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Penguin.Files.Embedded
+{
+    public static class EmbeddedFileService
+    {
+        public static byte[] ReadAllBytes(string path, Assembly source) {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentException($"'{nameof(path)}' cannot be null or empty", nameof(path));
+            }
+
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return TryExtract(path, source, File.ReadAllBytes); 
+        }
+
+        public static byte[] ReadAllBytes(string path)
+        {
+            if (path is null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            return ReadAllBytes(path, Assembly.GetCallingAssembly());
+        }
+
+        public static string ReadAllText(string path, Assembly source)
+        {
+            if (path is null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            return TryExtract(path, source, File.ReadAllText);
+        }
+
+        public static string ReadAllText(string path)
+        {
+            if (path is null)
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            return ReadAllText(path, Assembly.GetCallingAssembly());
+        }
+
+        private static byte[] ReadResource(Assembly assembly, string resourceName)
+        {
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (BinaryReader reader = new BinaryReader(stream))
+            {
+                return reader.ReadAllBytes();
+            }
+        }
+
+        private static T TryExtract<T>(string path, Assembly assembly, Func<string, T> toCall)
+        {
+            string resourceName = path.Replace("/", ".").Replace("\\", ".");
+
+            resourceName = $"{assembly.GetName().Name}.{resourceName}";
+
+            FileInfo resourceFile = new FileInfo(path);
+      
+            if (!resourceFile.Exists)
+            {
+                if(!resourceFile.Directory.Exists)
+                {
+                    resourceFile.Directory.Create();
+                }
+
+                File.WriteAllBytes(path, ReadResource(assembly, resourceName));
+            }
+
+            return toCall.Invoke(path);
+        }
+    }
+}
---
+++ Extensions/StreamExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace Penguin.Files.Embedded.Extensions
+{
+    internal static class StreamExtensions
+    {
+        public static byte[] ReadAllBytes(this BinaryReader reader)
+        {
+            // Pre .Net version 4.0
+            const int bufferSize = 4096;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] buffer = new byte[bufferSize];
+                int count;
+                while ((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+                {
+                    ms.Write(buffer, 0, count);
+                }
+
+                return ms.ToArray();
+            }
+        }
+    }
+}
