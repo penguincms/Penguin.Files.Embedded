9c3f6cecef9012c4a9a33ad3ceea00bb4f0f4491
AnLoffredo <38223101+AnLoffredo@users.noreply.github.com>
2021-07-08 14:51:56 -0700

    Autocommit
    Autocommit

--- ResourceDescriptor.cs
+++ ResourceDescriptor.cs
@@ -5,10 +5,21 @@ namespace Penguin.Files.Embedded
 {
     public class ResourceDescriptor
     {
+        /// <summary>
+        /// The assembly to search for the resource in. If defaults to executing
+        /// </summary>
         public Assembly SourceAssembly { get; set; } = Assembly.GetExecutingAssembly();
 
+
+        /// <summary>
+        /// Full path to the resource, overrides any other values
+        /// </summary>
         public string ResourceFullPath { get; set; }
 
+
+        /// <summary>
+        /// The namespace the resource is rooted in
+        /// </summary>
         public string Namespace { get; set; }
     }
 
@@ -16,14 +27,21 @@ namespace Penguin.Files.Embedded
     {
         public string SearchDirectory { get; set; }
 
-        public string GetSearchRoot()
-        {
-            return $"{(this.Namespace ?? this.SourceAssembly.GetName().Name)}.{this.SearchDirectory.Replace("\\", ".")}.";
-        }
+        public string GetSearchRoot() => $"{this.Namespace ?? this.SourceAssembly.GetName().Name}.{this.SearchDirectory.Replace("\\", ".")}.";
     }
 
+    /// <summary>
+    /// Defines how a resource should be extracted from an assembly, where it should be extracted to, and what assembly is being targeted
+    /// </summary>
     public class ResourceExtractionDescriptor : ResourceDescriptor
     {
+
+        public static implicit operator ResourceExtractionDescriptor(string path) => new ResourceExtractionDescriptor()
+        {
+            ResourceLocalPath = path,
+            OutputPath = path
+        };
+
         public ResourceExtractionDescriptor()
         {
         }
@@ -40,10 +58,15 @@ namespace Penguin.Files.Embedded
             this.ResourceFullPath = descriptor.ResourceFullPath;
         }
 
-        public string ResourcePath { get; set; }
-
+        /// <summary>
+        /// The path to the resource inside of the containing assembly
+        /// </summary>
         public string ResourceLocalPath { get; set; }
 
+        /// <summary>
+        /// Gets the full path including namespace, using the provided information
+        /// </summary>
+        /// <returns></returns>
         public string GetResourcePath()
         {
             if (this.ResourceFullPath != null)
@@ -56,6 +79,9 @@ namespace Penguin.Files.Embedded
             return $"{targetNs}.{this.ResourceLocalPath.Replace("\\", ".")}";
         }
 
+        /// <summary>
+        /// The file path that the embedded resource should be extracted to on disk
+        /// </summary>
         public string OutputPath { get; set; }
     }
 }
